bb53e1f4ab7f8fb3333cd4ab1817259d0d546689
Veeru <immaculateempyrean@gmail.com>
2022-01-14 18:02:42 +0530

    initialized the project by importing barebones custom scss.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomeSection />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeSection from '@/sections/HomeSection.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components:{
+        HomeSection
+    }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@use '@/assets/scss/setting' as *;
+
 </style>
